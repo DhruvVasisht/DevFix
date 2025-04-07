d09980172f405344f2c139a1c4c327791630a1c6
DhruvVasisht <vasisht.dhruv7@gmail.com>
2025-04-08 00:26:57 +0530

    Sidebar and navigation items Completed

--- components/action-tooltip.tsx
+++ components/action-tooltip.tsx
@@ -1,25 +1,35 @@
 "use client";
 
 import {
-    Tooltip,
-    TooltipContent,
-    TooltipTrigger,
-    TooltipProvider
-} from'@/components/ui/tooltip';
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  TooltipProvider,
+} from "@/components/ui/tooltip";
 
-
-interface ActionTooltipProps{
-    label:string;
-    children: React.ReactNode;
-    side?:"top" | "left" | "bottom" | "right";
-    alight?:"start" | "end" | "center";
+interface ActionTooltipProps {
+  label: string;
+  children: React.ReactNode;
+  side?: "top" | "left" | "bottom" | "right";
+  align?: "start" | "end" | "center";
 }
 
-export const ActionTooltip= ({
-    label,
-    children,
-    side,
-    alight 
-}:ActionTooltipProps) => {
-    
+export const ActionTooltip = ({
+  label,
+  children,
+  side,
+  align,
+}: ActionTooltipProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={50}>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent side={side} align={align}>
+          <p className="font-semibold text-sm capitalize">
+            {label.toLowerCase()}
+          </p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
 };
--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import { Plus } from "lucide-react";
+import { ActionTooltip } from "@/components/action-tooltip";
 
 const NavigationAction = () => {
   return (
+    <div>
+      <ActionTooltip label="Add a server" side="right" align="center">
     <button className="group flex items-center">
       <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] 
       transition-all overflow-hidden items-center justify-center bg-background 
@@ -11,6 +14,8 @@ const NavigationAction = () => {
         <Plus className="group-hover:text-white transition text-emerald-500" size={25}/>
       </div>
     </button>
+    </ActionTooltip>
+    </div>
   );
 };
 
